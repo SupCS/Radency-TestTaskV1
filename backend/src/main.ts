a2622600cff1b31cf61814b804fb6e715d30bf95
Dmytro Asparian <supezbiz@gmail.com>
2024-03-31 19:08:50 +0300

    changed cors settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+    app.enableCors({
+    origin: ['https://aspariankanban.netlify.app', 'http://localhost:3000'],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Accept",
+  });
   await app.listen(process.env.PORT || 3001);
-
 }
 bootstrap();
